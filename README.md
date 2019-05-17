14917d32346a466261794a583d50e5d3c7a2bad2
Lambda_School_Loaner_93 <antoniounivers@gmail.com>
2019-05-17 12:20:18 -0400

    mvp met

--- README.md
+++ README.md
@@ -29,9 +29,13 @@ Implement an User Authentication System in order to access the jokes from the Jo
 Demonstrate your understanding of this week's concepts by answering the following free-form questions. Edit this document to include your answers after each question. Make sure to leave a blank line above and below your answer so it is clear and easy to read by your project manager.
 
 1. What is the purpose of using _sessions_?
+-- session are used in order for the server to "remember who you are" so you won't have to relog in when you exit the page or refresh it. 
 1. What does bcrypt do to help us store passwords in a secure manner.
+-- bcrypt allos our passwords to be hashed--
 1. What does bcrypt do to slow down attackers?
+-- it encrypts are passwords  so that the become harder to decoded --
 1. What are the three parts of the JSON Web Token?
+-- Header. Payload. Signature. -- 
 
 ## Project Set Up
 
--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,9 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
+const db = require ('../user/user-model')
 
-const { authenticate } = require('../auth/authenticate');
+const { authenticate , jwtKey } = require('../auth/authenticate');
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -10,11 +13,52 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
-}
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
+  user.password = hash;
+
+  db.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+};
+
 
 function login(req, res) {
-  // implement user login
+  let { username, password } = req.body;
+  
+    db.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          const token = generateToken(user)
+          res.status(200).json({
+            message: `Welcome ${user.username}!  You have a token...`, token
+          });
+        } else {
+          res.status(401).json({ message: 'Invalid Credentials' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
 }
+function generateToken(user) {
+  const payload = {
+    subject:user.id, // this is what the token is about. it's about the users id
+    username: user.username,
+     //this puts the role on the token.  this would normally come from the database
+  
+  }
+  
+  const options = {
+    expiresIn: "1d",
+  }
+  return jwt.sign(payload,jwtKey,options) // this method runs in order
+  }
 
 function getJokes(req, res) {
   const requestOptions = {
